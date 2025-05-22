1541dcc601763f9efeaad2bbef2256365d693f79
Saikat Chakraborty <saikatc@microsoft.com>
2025-05-22 21:05:12 +0000

    Another reward

--- experimental/rewards.py
+++ experimental/rewards.py
@@ -3,6 +3,10 @@ import nltk
 from nltk.tokenize import word_tokenize
 from textwrap import dedent
 import re
+import copy
+import xml.etree.ElementTree as ET
+import textwrap
+
 problem_definition = """
 The type of the term is:
 ```
@@ -139,9 +143,65 @@ def step_answer_match(step, answer):
     return (float(l) / len(b_tokens)) if len(b_tokens) > 0 else 0
 
 
+def normalize_all_tags(text: str) -> str:
+    """
+    Normalize the text removing the tags and replacing them with sections.
+    First find the tags and then replace them with the corresponding sections.
+    Replace all the tags
+    """
+    def first_letter_uppercase(word):
+        return word[0].upper() + word[1:].lower()
+
+    def replace_tag(match):
+        tag = match.group(1)
+        return f"# {first_letter_uppercase(tag)}\n"
+
+    # Replace all tags with their normalized form
+    text = re.sub(r"[ \t]*<\s*([a-zA-Z0-9_]+)\s*>\s*", replace_tag, text)
+    text = re.sub(r"[ \t]*</\s*([a-zA-Z0-9_]+)\s*>\s*", "", text)
+    return text.strip()
+
+
+def xml_to_markdown(xml_string):
+    def format_text(text):
+        if not text or not text.strip():
+            return ""
+        return text
+
+    def parse_node(node, level=1):
+        md = ""
+        indent = "#" * level
+        title = node.tag.replace("_", " ").title()
+        md += f"\n{indent} {title}\n"
+        if node.text and node.text.strip():
+            md += f"{format_text(node.text)}\n"
+        children = list(node)
+        if children:
+            for i, child in enumerate(children, start=1):
+                md += parse_node(child, level + 1)
+        return md
+
+    root = ET.fromstring(xml_string)
+    markdown_output = parse_node(root)
+    markdown_output = re.sub(f"[\n]+", "\n", markdown_output)
+    markdown_output = re.sub(r"[ \t]+", " ", markdown_output)
+    lines = markdown_output.split("\n")
+    lines = [line.strip() for line in lines if line.strip()]
+    markdown_output = "\n".join(lines)
+    return markdown_output.strip()
+
+
+def try_parse_and_normalize_with_xml_parser(_think_block: str) -> str:
+    think_block = copy.deepcopy(_think_block)
+    think_block = f"<Reasoning>{think_block}</Reasoning>"
+    try:
+        return xml_to_markdown(think_block)
+    except ET.ParseError:
+        return None
+        
+
 def parse_think_xml_with_optional_blocks(xml_text: str):
     xml_text = dedent(xml_text.strip())
-
     def extract_block(tag: str) -> str:
         """Extracts a single block like <reflection>...</reflection>"""
         pattern = fr"<{tag}>\s*(.*?)\s*</{tag}>"
@@ -154,11 +214,25 @@ def parse_think_xml_with_optional_blocks(xml_text: str):
         step_pattern = r"<step>\s*(.*?)\s*</step>"
         return [s.strip() for s in re.findall(step_pattern, block, re.DOTALL | re.IGNORECASE)]
 
+    def extract_think_block() -> str:
+        text = copy.deepcopy(xml_text)
+        if "<think>" in xml_text:
+            text = text[(text.index("<think>") + len("<think>")):]
+        if "</think>" in text:
+            text = text[:text.index("</think>")]
+        return text.strip()
+
+    think_block = extract_think_block()
+    normalized_text = try_parse_and_normalize_with_xml_parser(think_block)
+    if normalized_text is None:
+        normalized_text = normalize_all_tags(think_block)
+
     return {
         "reflection": sanitize(extract_block("reflection")),
         "steps": [sanitize(s) for s in extract_step_list("steps")],
         "emulation": [sanitize(s) for s in extract_step_list("emulation")],
         "answer": sanitize(extract_block("answer")),
+        "normalized_think_block": normalized_text,
     }
 
 
@@ -173,19 +247,21 @@ class FormattedResponse:
                 "Invalid problem prefix. Expected 'fstar' or 'verus'.")
         self.problem_description = problem_desciption
         self.original_text = response
-        self.reflection = ""
-        self.steps = []
-        self.emulation = []
-        self.answer = ""
 
         result = parse_think_xml_with_optional_blocks(response)
         self.reflection = result["reflection"]
         self.steps = result["steps"]
         self.emulation = result["emulation"]
         self.answer = result["answer"]
-        self.client = openai.OpenAI(
-            base_url="http://localhost:8000/v1", api_key="sk-")
-        self.model = self.client.models.list().data[0].id
+        self.full_reasoning = result["normalized_think_block"]
+        try:
+            self.client = openai.OpenAI(
+                base_url="http://localhost:8000/v1", api_key="sk-")
+            self.model = self.client.models.list().data[0].id
+        except Exception as e:
+            self.client = None
+            self.model = None
+            print("Error initializing OpenAI client:", e)
 
     def __str__(self):
         ret_val = f"""Reflection:
@@ -247,21 +323,21 @@ class FormattedResponse:
 
     def call_model_with_retry(self, prompt, max_trial=10, temparature=0.0):
         while max_trial > 0:
-            response = self.client.chat.completions.create(
-                model=self.model,
-                messages=prompt,
-                max_tokens=5000,
-                temperature=temparature,
-            )
-            response_text = response.choices[0].message.content
-            # Extract the score from the response
             try:
+                response = self.client.chat.completions.create(
+                    model=self.model,
+                    messages=prompt,
+                    max_tokens=5000,
+                    temperature=temparature,
+                )
+                response_text = response.choices[0].message.content
+                # Extract the score from the response
                 score = self.find_score_in_response(response_text)
-                # print("====================")
-                # print(response_text)
-                # print("====================")
+                print("====================")
+                print(response_text)
+                print("====================")
                 return score
-            except ValueError as e:
+            except Exception as e:
                 max_trial -= 1
                 temparature += 0.1
 
@@ -275,7 +351,7 @@ class FormattedResponse:
         prompt = [
             {
                 "role": "system",
-                "content": "You are a helpful assistant that is experiences with logical reasoning. The user is trying to solve a problem related to synthesizing a verifiable code from a given code or specification. In order to do so, the user came up with a list of steps. The user also has simulated these steps in their mind and articulated the simulation in a natural language. The user is asking you to check if the steps and the emulation are conforming to each other. Please answer with a score between 0 and 1, where 0 means they do not conform at all and 1 means they are exactly the same. You should first think about the conformity between the steps and the emulation. If there is a conformity, think about the reasoning behind it. If there is a disagreement, think about the reason why they do not conform. Then, give a score between 0 and 1. You should format your answer in <think> provide detailed reasoning here </think> <score> score goes here </score> format. Do not include any other text in your answer.",
+                "content": "You are a helpful assistant that is experiences with logical reasoning. The user is trying to solve a problem related to synthesizing a verifiable code from a given code or specification. In order to do so, the user came up with a list of steps. The user also has simulated these steps in their mind and articulated the simulation in a natural language. The user is asking you to check if the steps and the emulation are conforming to each other. Please answer with a score between 0 and 1, where 0 means they do not conform at all and 1 means they are exactly the same. You should first think about the conformity between the steps and the emulation. If there is a conformity, think about the reasoning behind it. If there is a disagreement, think about the reason why they do not conform. Then, give a score between 0 and 1. You should also articulate your thought process for the score you wil give. You should format your answer in \n<think>\n\tprovide your thought process of scoring\n</think>\n<score>\n\tYour given score between 0 and 1\n</score>\n format. Do not include any other text in between <score> and </score> tags.",
             }
         ]
         user_prompt = f"{self.problem_prefect}\n\n" + \
@@ -295,6 +371,29 @@ class FormattedResponse:
             }
         )
         return self.call_model_with_retry(prompt)
+    
+    def full_reasoning_lead_to_answer_score(self) -> float:
+        # Here we will check if given a problem definition, the developers reasoning does indeed reach the correct answer. If we can exactly reach the answer, we will return 1.0, if we can reach a similar answer, we will return a score between 0 and 1. If we cannot reach the answer at all, we will return 0.0.
+        # Create a prompt for the model
+        prompt = [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that is experiences with logical reasoning. The user is trying to solve a problem related to synthesizing a verifiable code from a given code or specification. In order to do so, the user came up with some reasoning. The user is asking you to check if the reasoning leads to the answer. Please answer with a score between 0 and 1, where 0 means you cannot derive the answer following the reasoning and 1 means you can derive the answer by following the reasoning. While scoring the reasoning, you should consider the following.\n1. If the reasoning is logically sound, a sound reasoning should get higher score than unsound one\n2. If the reasoning is aligned with solving the problem and if the reasonign is making unjust assumption. A reasoning that aligns with solving the problem with fewer unjust assumption should get higher score.\n3. If the reasoning does indeed reach to the solution. If someone with no experience could independently follow the reasoning process and reach to the solution that reasoning should get higher score.\n You should also articulate your thought process for the score you wil give. You should format your answer in \n<think>\n\tprovide your thought process of scoring\n</think>\n<score>\n\tYour given score between 0 and 1\n</score>\n format. Do not include any other text in between <score> and </score> tags.",
+            }
+        ]
+        user_prompt = f"{self.problem_prefect}\n\n" + \
+            self.problem_description + "\n"
+        user_prompt += "\nThe developers reasoning is: "
+        for i, step in enumerate(self.steps):
+            user_prompt += f"\nStep {i + 1}: {step}"
+
+        prompt.append(
+            {
+                "role": "user",
+                "content": user_prompt,
+            }
+        )
+        return self.call_model_with_retry(prompt)
 
     def step_answer_conformity_score(self) -> float:
         # Here we will check if given a problem definition, and list of steps, if we could reach the answer. If we can exactly reach the answer, we will return 1.0, if we can reach a similar answer, we will return a score between 0 and 1. If we cannot reach the answer at all, we will return 0.0.
@@ -302,7 +401,7 @@ class FormattedResponse:
         prompt = [
             {
                 "role": "system",
-                "content": "You are a helpful assistant that is experiences with logical reasoning. The user is trying to solve a problem related to synthesizing a verifiable code from a given code or specification. In order to do so, the user came up with a list of steps. The user alse has prepared an answer. The user is asking you to check if following the steps, the user could actually reach the answer. Please answer with a score between 0 and 1, where 0 means you cannot derive the answer following the steps and 1 means you can derive the answer by following the steps. While scoring the steps, you should think about the logical deduction for each of the steps and how each of these step transform the problem. You should deeply reason about why the list of given steps are correct or not to reach the answer. Then, give a score between 0 and 1. You should format your answer in <think> provide detailed reasoning here </think> <score> score goes here </score> format. Do not include any other text in your answer.",
+                "content": "You are a helpful assistant that is experiences with logical reasoning. The user is trying to solve a problem related to synthesizing a verifiable code from a given code or specification. In order to do so, the user came up with a list of steps. The user alse has prepared an answer. The user is asking you to check if following the steps, the user could actually reach the answer. Please answer with a score between 0 and 1, where 0 means you cannot derive the answer following the steps and 1 means you can derive the answer by following the steps. While scoring the steps, you should think about the logical deduction for each of the steps and how each of these step transform the problem. You should deeply reason about why the list of given steps are correct or not to reach the answer. Then, give a score between 0 and 1. You should also articulate your thought process for the score you wil give. You should format your answer in \n<think>\n\tprovide your thought process of scoring\n</think>\n<score>\n\tYour given score between 0 and 1\n</score>\n format. Do not include any other text in between <score> and </score> tags.",
             }
         ]
         user_prompt = f"{self.problem_prefect}\n\n" + \
@@ -324,6 +423,7 @@ class FormattedResponse:
 # Example usage
 fmt = FormattedResponse(problem_prefect="fstar",
                         problem_desciption=problem_definition, response=resp)
+            
 # print(fmt)
 print("Is well-formed (Reflection Only): ",
       fmt.is_well_formed(reflection_expected=True))
@@ -340,3 +440,5 @@ print("Is well-formed (All): ", fmt.is_well_formed(reflection_expected=True,
       steps_expected=True, emulation_expected=True))
 print("Step Emulation match emulation:", fmt.step_emulation_conformity_score())
 print("Step Answer conformity check:", fmt.step_answer_conformity_score())
+print("Full reasoning lead to answer score:", fmt.full_reasoning_lead_to_answer_score())
+
